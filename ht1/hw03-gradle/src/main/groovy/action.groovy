c082445fa965ff61b63910b3f57a734d44110f70
Илья Гвоздков <ilya.gvozdkov@gazprombank.ru>
2023-09-13 00:13:40 +0300

    hometask 3. upd

--- ht1/hw03-gradle/src/main/groovy/action.groovy
+++ ht1/hw03-gradle/src/main/groovy/action.groovy
@@ -7,4 +7,11 @@ class Action {
     LocalDateTime startTime
     LocalDateTime endTime
     Long taskId
+
+    def showEvent(LocalDateTime dt){
+        if(dt==endTime){
+            Event evt = new Event(message: "action ${name} finished", created: endTime)
+            println(evt)
+        }
+    }
 }
